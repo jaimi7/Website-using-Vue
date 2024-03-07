3250c4249ecbbf6aaa98c06d47f3e79b4a1b3d3c
“Jaimi <jaimip@zignuts.com>
2024-03-07 17:06:32 +0530

    Create full courses page

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import SubHeaderMenu from "@components/layout/SubHeaderMenu.vue"
 import FooterBox from "@components/layout/FooterBox.vue"
+import CourseBox from "@components/home/CourseBox.vue"
+import TestimonialsBox from "@components/home/TestimonialsBox.vue"
 </script>
 
 <template>
   <div>
+    <!-- header  -->
     <SubHeaderMenu heading="Our Courses" />
+
+    <!-- course -->
+    <CourseBox />
+
+    <!-- testimonials -->
+    <TestimonialsBox />
+
+    <!-- footer  -->
     <FooterBox />
   </div>
 </template>
