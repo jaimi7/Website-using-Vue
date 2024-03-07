52d010c3f981dcdb15e46b3f6c6e8413291379ba
“Jaimi <jaimip@zignuts.com>
2024-03-07 17:04:53 +0530

    Create full about page

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about_us">
+        <div class="row">
+            <div class="about_us_col">
+                <h2>We are the world's largest university</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque est modi commodi in dolores
+                    deserunt! Porro cum molestiae fuga dolor tenetur omnis beatae iure aliquid veritatis nesciunt! Autem
+                    illo provident tempore sed molestiae officia aut quo, excepturi soluta dolore quidem, amet vero
+                    deleniti velit blanditiis omnis vitae aliquam, praesentium nostrum repellat accusamus itaque ex
+                    ullam! Totam, dicta dolorum.</p>
+                <a href="" class="explore_btn">Explore Now</a>
+            </div>
+            <div class="about_us_col">
+                <img src="@/assets/img/about.jpg" alt="About">
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import SubHeaderMenu from "@components/layout/SubHeaderMenu.vue"
 import FooterBox from "@components/layout/FooterBox.vue"
+import AboutUs from "@components/AboutUs.vue"
 </script>
 
 <template>
   <div>
+    <!-- header -->
     <SubHeaderMenu heading="About Us" />
+
+    <!-- about us  -->
+    <AboutUs />
+
+    <!-- footer  -->
     <FooterBox />
   </div>
 </template>
